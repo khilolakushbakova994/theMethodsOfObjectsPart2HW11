3724a524a4ecf90fa06a96355302d923ba4eb223
khilola.kushbakova <xilola_kushbakova@mai.ru>
2022-11-01 16:46:32 +0200

    changes made

--- src/Book.java
+++ src/Book.java
@@ -1,22 +1,21 @@
-import java.util.Objects;
-
 public class Book {
-    private final String authorName;
     private final String bookName;
+    private final Author authorName;
     private int publishingYear;
 
-    public Book(String bookName, String authorName, int publishingYear) {
+    public Book(String bookName, Author authorName, int publishingYear) {
         this.bookName = bookName;
         this.authorName = authorName;
         this.publishingYear = publishingYear;
     }
 
-    public String getBookName() {
-        return bookName;
+    public  String toString () {
+        return "Название книги  " + bookName + "\n " + authorName + "\n Год выпуска книги   " + publishingYear;
     }
 
-    public String getAuthorName() {
-        return authorName;
+    public String getBookName() {
+        return bookName;
+
     }
 
     public int getPublishingYear() {
@@ -27,24 +26,9 @@ public class Book {
         this.publishingYear = 2022;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Book book = (Book) o;
-        return publishingYear == book.publishingYear && Objects.equals(authorName, book.authorName) && Objects.equals(bookName, book.bookName);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(authorName, bookName, publishingYear);
-    }
-
-    public String toString () {
-        return "Название книги  " + bookName + "\n Писатель  " + authorName + "\n Год выпуска книги   " + publishingYear;
-
-    }
 
 }
+
+
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        String author2 = new String("Dan Brown");
+
         Author author = new Author("Guillaume", "Musso");
+        Book book2 = new Book ( "After", author, 2003);
+        Author author2 = new Author("Dan" , "Brown");
         Book book = new Book("The Da Vinci Code", author2, 2003);
-        System.out.println("Название книги  "+ book.getBookName());
-        System.out.println("Автор книги  " + book.getAuthorName());
-        System.out.println("Год первой печати книги  " + book.getPublishingYear());
 
         System.out.println( author.toString());
+        System.out.println();
         System.out.println( book.toString());
-
 
 
     }
